e2c42c1c777ed5028ca29960cc0259918a988627
WHorrocks <WHorrock@UCSD.edu>
2017-11-17 14:24:51 -0800

    Non-modular ANN

--- ANN.py
+++ ANN.py
@@ -8,115 +8,25 @@ from TensorGenerator import TensorGenerator
 
 
 
-#First command line argument is the path to the file that stores the desired data.
-#Second command line argument is the number of points for one of the shapes (Total number of points/2)
-#Third command line argument is the dimensionality of the problem under consideration (i.e. N=?)
+
+#First command line argument is the path to the circle CSV, second command line argument is the path to the torus CSV.
 
 
-*****CHANGE HERE BY USING TF DATASET API IN ORDER TO PROCESS DATA. ASSIGN THE PREPROCCESSING TO THE CPU TO FREE UP THE GPU(S) FOR TRAINING.THEREFORE, SPECIFY THAT THE CPU IS TO BE USED TO FORMAT THE DATA IN A TF.TENSOR OBJECT SO THAT THE ANN CAN WORK WITH IT*****
+
+#Configured for No Overlap (TorusRadius=60). Change this if needed here:
+TorusRadius=60
+
+
 #############################################################################################################################################
-#STEP ONE: READ IN DATA. Using tf.device to specify that the CPU is used to preprocess the data.
+#STEP ONE: READ IN DATA
 
 
+#Gives me two numpy arrays. One of them is NumDataPoints by 2 array (each row has an x and y coordinate) and the other is NumDataPoints by 1 array that just contains the labels.
+ToGenerateData=TensorGenerator()
 
-#STEPS:
-#1) Assign the directions to the cpu via the tf.device statement
-#2) Set up a queue of file names (can put more files in here if desired later on)
-#3) Initialize a tf.TextLineReader to read the data from the CSV
-#4) Tell the reader what file to read via a read statement (the file is automatically dequeued once it has hit the end of file) 
-#5) Use tf.decode_csv to go through one line and assign each comma seperated value to a tensor object.  
-#6) Customizing the configuration of the data in the list and stacking all of the tensor objects in the list into one big tensor ([Coord1,Coord2,...,CoordN,[Label]]) to get it ready for addition to the Dataset. 
-#7) If it is the first iteration (that is, it is the first line being read) then create the Dataset structure (named InputAndLabels) with the tensor that contains all the coordinates and the label as the first entry. If it is not the first iteration, just concatenate the tensorwith all the CSV information to the pre-existing Dataset object (which is named InputAndLabels)
-#8) Running a session to format all of the data.
+#train_x is the training coords, train_y is the training labels, test_x is the testing coords, test_y is the testing labels. 
+train_x,train_y,test_x,test_y,NumOfDataPoints=ToGenerateData.OutputData(sys.argv[1],sys.argv[2])
 
-FirstRun=true;
-Dimensionality=sys.argv[3]
-
-
-#Step 1)
-with tf.device('/cpu:0'):
-	
-	#Step 2)
-	#Can put in more files if desired here.
-	QueueOfFiles=tf.train.string_input_producer([sys.argv[1]])
-
-	#Step 3)
-	#Setting up a reader operation.	
-	Reader=tf.TextLineReader()
-	
-	#Num Data Points variable to keep track of how many data points to expect.
-	NumDataPoints=int(sys.argv[2])
-
-
-	#Step 4)
-	#Outputs used to identify file
-	_,Row=Reader.read(QueueOfFiles)
-
-	
-
-
-	#Setting up default array so the reader knows what to expect. Using a negative number much larger than what we use for troubleshooting later.
-	GenDataPoint=tf.constant(-9999,dtype=tf.float32)
-	GenLabel=tf.constant(-1,dtype=tf.int32)
-	Default=[]
-	for x in range(0,NumDataPoints):
-		Default.append[[GenDataPoint]]
-	Default.append([[GenLabel]])
-	
-
-	#Step 5)
-	#Setting up an operation that reads a single line from the CSV. This returns a list of length N+1 that contains all of the elements in one line of the CSV. That is: Coord1,Coord2,Coord3,...Label.
-	CSV_Values=tf.decode_csv(Row,record_defaults=Default)
-
-	#Step 6)
-	#Configuring the lists
-	EndPoint=length(InputData)-2
-	DataPointList=CSV_Values[:EndPoint]
-	Label=CSV_Values[EndPoint:]
-	
-	#Format of NewList is [Coord1,Coord2,Coord3,...CoordN,[Label]]
-	NewList=DataPointList.append([Label])
-
-
-	#Step 7)
-	#Stacking all of the returned data into one tensor object. This tensor object will be put into a Dataset object that contains all of the coordinates we are training the ANN on and the Label in its own category as a seperate list (see NewList comment).
-	OneCompleteCoordAndLabel=tf.stack(NewList)
-	
-
-	if FirstRun:
-		InputAndLabels=Dataset.from_tensors(OneCompleteCoordAndLabel)
-		FirstRun=false
-	else:
-		InputAndLabels.concatenate(OneCompleteCoordAndLabel)
-
-
-		
-	#Step 8)
-	with tf.Session() as sess:
-		#Starting all the threads on the graph so that QueueOfFiles is filled.
-		coord=tf.train.Coordinator()
-		threads=tf.train.start_queue_runners(coord=coord)
-
-		
-		for i in range(0,NumDataPoints*2): 
-			#Getting the whole list of tensors.			
-
-			DataGen=sess.run([OneCompleteCoordAndLabel])
-			if i==0:
-                		TrainingInputAndLabels=Dataset.from_tensors(DataGen)
-                		FirstRun=false
-        		else:
-				if i<=(NumDataPoints*2)*0.8:
-                			TrainingInputAndLabels.concatenate(DataGen)
-				else:
-					if i==((NumDataPoints*2)*0.8):
-						ValidationInputAndLabels.Dataset.from_tensors(DataGen)
-					else:
-						ValidationInputAndLabels.concatenate(DataGen)
-				
-
-
-		
 
 #############################################################################################################################################
 
@@ -124,7 +34,7 @@ with tf.device('/cpu:0'):
 
 
 
-*****BASIC SETUP OF THE ANN. LEAVE AS IS*****
+
 #############################################################################################################################################
 #STEP TWO: SET UP THE CONSTANTS OF THE NN
 
@@ -135,8 +45,10 @@ num_Nodes=10
 #Number of output nodes
 n_classes=2
 
+#Batch Size
+batch_size=100
 
-#x is input (the N dimensional coordinate). y is labels.
+#x is input (the x-coordinate and y-coordinate). y is labels. These are placeholders I will feed things through later on (Will feed the data to be analyzed through x and the labels through y)
 x=tf.placeholder('float')
 y=tf.placeholder('float')
 
@@ -147,16 +59,16 @@ y=tf.placeholder('float')
 
 
 
-*****BASIC SETUP OF THE ANN. LEAVE AS IS*****
+
 #############################################################################################################################################
 #STEP THREE: SETTING UP THE ACTUAL LAYER(S) OF THE ANN.
 
 
-#Setting up the computation graph of the ANN. 
+#Setting up the computation graph of the ANN. The ANN has two inputs (the x-coordinate and the y-coordinate of the data point), one layer with the user defined number of nodes, and one output layer. "data" is what I feed to the neural network. 
 def neural_network_model(data):
 
 	#The weights are randomized for the first run through. The weights will be calibrated to minimize the cost function. 
-	hidden_1_layer={'weights':tf.Variable(tf.random_normal([Dimensionality,num_Nodes])),'biases':tf.Variable(tf.random_normal([num_Nodes]))}
+	hidden_1_layer={'weights':tf.Variable(tf.random_normal([2,num_Nodes])),'biases':tf.Variable(tf.random_normal([num_Nodes]))}
 	
 	#These are the weights that are leading into the two output nodes.
 	output_layer={'weights':tf.Variable(tf.random_normal([num_Nodes,n_classes])),'biases':tf.Variable(tf.random_normal([n_classes]))}
@@ -181,7 +93,8 @@ def neural_network_model(data):
 
 
 
-#Assuming the dataset is made from here on out.
+
+
 #############################################################################################################################################
 #STEP THREE: TRAINING THE NEURAL NETWORK
 
@@ -202,20 +115,13 @@ def train_neural_network(x):
 	#Optimizing function that acts to minimize the cost function (and therefore minimize the difference between the output values and the expected values)
 	optimizer = tf.train.AdamOptimizer().minimize(cost)
 
+	#One epoch is a complete cycle through the data as well as the backpropogation (adjusting the weights to increase accuracy)
+	num_epochs=50
 
 	#Setting up the accuracy array to hold the ANN's accuracy per epoch.
         AccuracyArray=np.zeros(num_epochs)
 		
-
-	#Making the iterator
-	iterator=InputAndLabels.make_initializable_iterator()
-	next_element=iterator.get_next()
-
-
-	#Making the constants to feed the iterator.
-	TrainingMaxVal=tf.constant	
-
-
+	
 	#Now actually running the ANN.
 	with tf.Session() as sess:
 
@@ -229,18 +135,33 @@ def train_neural_network(x):
                 correct=tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
                 accuracy=tf.reduce_mean(tf.cast(correct,'float'))
 
+		for epoch in range(num_epochs):
+			epoch_loss = 0
+			i=0
+			batch_num=0
+
 	
+			#Takes batches of the training data.
+			while i < len(train_x):
+				start=i
+				end=i+batch_size
+				batch_x=np.array(train_x[start:end])
+				batch_y=np.array(train_y[start:end])
+
+
+
+				#This is where the ANN is actually ran. The weights and biases are modified with this line of code (since the optimizer operation is ran).
+				_,c,ToPrint=sess.run([optimizer,cost, prediction], feed_dict={x: batch_x, y: batch_y})
+				epoch_loss+=c
+				correct=tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+                		accuracy=tf.reduce_mean(tf.cast(correct,'float'))
+				i+=batch_size
 			
+			#Getting the accuracy per epoch
+			AccuracyArray[epoch]=accuracy.eval({x:test_x, y:test_y})*100
 
-
-
-
-		#Need iterating/training code here
-
-
-
-
-	
+			epochNum=epoch+1
+		
 		#Getting the max accuracy achieved as well as the epoch it achieved that accuracy.
 		MaxAccuracyAchieved=np.amax(AccuracyArray)
 		EpochMaxAccuracy=np.argmax(AccuracyArray)+1
@@ -251,7 +172,9 @@ def train_neural_network(x):
 
 
 
-#Slight modification of how accuracy is determined needed
+
+
+
 #############################################################################################################################################
 #STEP FOUR: TESTING THE ACCURACY OF THE NEURAL NETWORK 
 
@@ -263,18 +186,17 @@ def train_neural_network(x):
 		correct=tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
 		accuracy=tf.reduce_mean(tf.cast(correct,'float'))
 		
-		#Writing results to a file
-		FileName=str(sys.argv[1])
-		with open(FileName,"a") as f:
-			f.write(MaxAccuracyAchieved,EpochMaxAccuracy)
-		 
+		#Printing a message indicating the accuracy the ANN achieved as well as how many epochs it required.
+		print "%f,%d" %(MaxAccuracyAchieved,EpochMaxAccuracy)
+
 		
 #############################################################################################################################################
 
 
 
 
-#Good to go 
+
+
 #############################################################################################################################################
 #STEP FIVE: RUNNING THE CODE
 
