6f168dbce774233c656a61b11fa1d10195b39a19
Naveen Kashyap naveen@thekashyaps.net 10/23/2017 <naveen@thekashyaps.net>
2017-11-29 11:10:34 -0800

    results from varying outer sphere mu from 15-25

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NODES=50
-OSMU=10
+OSMU=15
 OSSIG=5
 ISMU=0
 ISSIG=5
@@ -13,17 +13,23 @@ while [ $NODES -lt 201 ]
 do
 
 LAYERS=1
-
   while [ $LAYERS -lt 11 ]
   do
     NODES_PER_LAYER=$((NODES / LAYERS))
 
-    echo "Using $LAYERS layer(s), $NODES_PER_LAYER nodes per layer, $DIMENS dimensions, $POINTS points per sphere for $EPOCHS epochs"
-    echo "Total number of nodes: $NODES"
-    echo Outer sphere: mu = $OSMU sigma=$OSSIG
-    echo Inner sphere: mu = $ISMU sigma=$ISSIG
+    OSMU=15
+    while [ $OSMU -lt 26 ]
+    do
+      
+      echo "Using $LAYERS layer(s), $NODES_PER_LAYER nodes per layer, $DIMENS dimensions, $POINTS points per sphere for $EPOCHS epochs"
+      echo "Total number of nodes: $NODES"
+      echo Outer sphere: mu = $OSMU sigma=$OSSIG
+      echo Inner sphere: mu = $ISMU sigma=$ISSIG
 
-    singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python3 ANN.py $DIMENS $POINTS $OSMU $OSSIG $ISMU $ISSIG $NODES_PER_LAYER $EPOCHS $LAYERS > /dev/null
+      singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python3 ANN.py $DIMENS $POINTS $OSMU $OSSIG $ISMU $ISSIG $NODES_PER_LAYER $EPOCHS $LAYERS > /dev/null
+    OSMU=$((OSMU+5))
+
+    done
 
     LAYERS=$((LAYERS+1))
 
