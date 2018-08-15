9becd3b8e3e9e16862ba06468fd6e3bb2a1ecce6
WHorrocks <WHorrock@UCSD.edu>
2018-08-14 21:26:40 -0700

    Runs the ANN
    Command Line Arguments:
    1) Area
    2) Points Per Dist
    3) Square Distribution Sigma
    4) Distributions per Side (Square)
    5) Circle Sigma
    6) num_Nodes
    7) num_epochs
    8) num_layers
    
    This is configured so that each run has roughly the same number of parameters.

---
+++ circle_square_W18/New_And_Old_Square_Gen/OldSquareGen/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 415 100 1`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 43 100 2`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 31 100 3`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 25 100 4`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 22 100 5`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 19 100 6`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 18 100 7`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 16 100 8`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 15 100 9`
+`singularity exec --bind /usr/lib64/nvidia:/host-libs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/tensorflow-gpu:latest python ANN.py 20 50 0.1 50 1 14 100 10`
